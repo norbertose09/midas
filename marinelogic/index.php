041086f046a9a8e1b1f888abe948d8a32aad415d
norbertose09 <91324184+norbertose09@users.noreply.github.com>
2022-06-15 16:17:40 +0100

    Add files via upload

---
+++ marinelogic/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>TheEvent Bootstrap Template - Index</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  
+</head>
+<body> 
+  <center>
+<h1 style="color:grey;">Work in Progress...</h1>
+<!-- <div class="col-lg-4 order-1 order-lg-2 hero-img" data-aos="zoom-out" data-aos-delay="300"> -->
+          <img src="../assets/img/undraw_Yacht_re_kkai.png" class="img-fluid animated" alt="" style="height:220px; width:320px;">
+        <!-- </div> -->
+        <br>
+<a href="<?php echo '../'   ?>" style="font-family:georgia; color
+:black; font-face:bold;">Go Back Home</a></center>
+</body>
